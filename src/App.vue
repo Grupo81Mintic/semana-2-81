52fe7c9b72aeef7d839492c6aaa9763b48b38410
Louis Rex <rlhardrock@gmail.com>
2020-12-05 22:42:37 -0500

    servicios vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
   <div id="app">
+
+
+    <a name="servicios"></a>
+    <div class="card-deck" v-for="(item,index) of servicios" :key="index">
+      <ServiciosData v-bind:servicio="item"></ServiciosData>
+    </div>
+
+
+
   </div>
 </template>
 
 <script>
 
+import ServiciosData from './components/ServiciosData.vue'
+
+
 export default {
   name: 'App',
-  componment
+  components: {
+    ServiciosData
+  },
   data(){
   return{
   title
+  }
+ }
 }
 </script>
 
---
+++ src/components/ServiciosData.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card">
+    <img v-bind:src="servicio.imagen" class="card-img-top" alt="..." height="300">
+        <div class="card-body">
+            <div class="card-header">{{servicio.servicio}}</div>
+            <p class="card-text">{{servicio.detalle}}</p>
+                <a href="servicio.url" class="btn btn-secondary">{{servicio.enlace}}</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'ServiciosData',
+    props : ['servicio']
+}
+</script>
+
+
+<style scoped>
+
+</style>
